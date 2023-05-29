45dc54322a626785d550392fdee8f0a4f1089201
Viktoriia <vichka53@gmail.com>
2023-05-30 01:53:03 +0300

    add Task39

---
+++ Task39/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] createArrayRndInt = CreateArrayRndInt(5, 1, 9);
+Console.Write("[");
+PrintArray(createArrayRndInt);
+Console.Write("] -> ");
+ReverseArray(createArrayRndInt);
+Console.Write("[");
+PrintArray(createArrayRndInt);
+Console.Write("]");
+// Array.Reverse(createArrayRndInt);
+// Console.Write("[");
+// PrintArray(createArrayRndInt);
+// Console.Write("]");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1- i];
+        arr[arr.Length - 1- i] = temp;
+    }
+}
