6f357cd03350f52c0003c8e6519f120d46c34f9e
Viktoriia <vichka53@gmail.com>
2023-06-14 02:03:39 +0300

    add Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 55: Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] createMatrix = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(createMatrix);
+if (ValueMatrix(createMatrix))
+{
+    ChangePosition(createMatrix);
+    Console.WriteLine();
+    PrintMatrix(createMatrix);
+}
+else Console.WriteLine("Невозможно заменить строки на столбцы, так как матрица не квадратная.");
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool ValueMatrix(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+void ChangePosition(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+
+}
