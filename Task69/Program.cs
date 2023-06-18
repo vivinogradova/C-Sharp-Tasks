394f421f231d9bfce10e6efda11af3d0bf2d90f1
Viktoriia <vichka53@gmail.com>
2023-06-19 01:18:29 +0300

    add Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите натуральное число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponenta = Exponenta(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} -> {exponenta}");
+
+
+int Exponenta(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * Exponenta(a, b - 1);
+}
+
+
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(10)); // 3628800
